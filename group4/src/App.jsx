6d096e3ef6ea2e958c88623392b62acf073efc13
LAPTOP-30EMCOCT\nunuaung <nunuaung942@gmail.com>
2023-09-09 19:11:17 +0630

    initial commit

---
+++ group4/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='content'>
+      <h1 className="text-3xl font-bold underline text-center bg-gray-500">Hello world!</h1> 
+    </div>
+  )
+}
+
+export default App
